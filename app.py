5db1f8a79020123981daead09c1b179460fdb805
ufukfx <127700400+ufukfx@users.noreply.github.com>
2023-03-12 23:59:56 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import json
+from serpapi import GoogleSearch
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def search_results():
+    # Set the search query parameters
+    params = {
+      "api_key": "b9cd1a55978beb9b880e5ed669a9b0ff86d96b26ec87d3be5defe9080c55847b",
+      "engine": "google",
+      "q": "bahis siteleri ",
+      "location": "Ankara, Ankara, Turkey",
+      "google_domain": "google.com.tr",
+      "gl": "tr",
+      "hl": "tr",
+      "device": "mobile"
+    }
+
+    # Make the API request and get the JSON response
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    # Extract the search results and create the HTML table
+    table_rows = ''
+    for result in results['organic_results']:
+        position = result['position']
+        title = result['title']
+        link = result['link']
+        displayed_link = result['displayed_link']
+        row = f'<tr><td>{position}</td><td>{title}</td><td><a href="{link}">{displayed_link}</a></td></tr>'
+        table_rows += row
+
+    # Render the HTML template with the search results
+    return render_template('search_results.html', table_rows=table_rows)
+
+if __name__ == '__main__':
+    app.run(debug=True)
